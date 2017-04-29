687b8693e408745ed10adcfaa732b31eeee59542
Ryan J. Popovic <Ryan J. Popovic>
2017-04-29 13:04:17 -0400

    Checkpoint 8 Services Part 3: Completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,16 +1,19 @@
 (function() {
-  function SongPlayer() {
+  function SongPlayer(Fixtures) {
     var SongPlayer = {};
+
     /**
-    *@desc Currently selected song
-    *@param {Object}
+    *@desc Retrieves album information from Fixtures service
+    *@type {Method}
     */
-    var currentSong = null;
+    var currentAlbum = Fixtures.getAlbum();
+
     /**
     *@desc Buzz object audio file
     * @type {Object}
     */
     var currentBuzzObject = null;
+
     /**
     * @function setSong
     * @desc Stops currently playing song and loads new audio file as currentBuzzObject
@@ -19,7 +22,7 @@
     var setSong = function(song) {
       if (currentBuzzObject) {
         currentBuzzObject.stop();
-        currentSong.playing = null;
+        SongPlayer.currentSong.playing = null;
       }
 
       currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -27,8 +30,9 @@
         preload: true
       });
 
-      currentSong = song;
+      SongPlayer.currentSong = song;
     }
+
     /**
     *@function playSong
     *@desc Plays audio file currentBuzzObject
@@ -38,6 +42,23 @@
       currentBuzzObject.play();
       song.playing = true;
     }
+
+    /**
+    *@function getSongIndex
+    *@desc Retrieves songs index from list of songs
+    *@param {Object} song
+    *@returns {Number}
+    */
+    var getSongIndex = function(song) {
+      return currentAlbum.song.indexOf(song);
+    }
+
+    /**
+    *@desc Active song object from list of songs
+    *@param {Object}
+    */
+    SongPlayer.currentSong = null;
+
     /**
     *@function SongPlayer.play
     *@desc If current song is not the song user clicks on, the clicked song becomes current song and plays.
@@ -45,31 +66,54 @@
     *@param {Object} song
     */
     SongPlayer.play = function(song) {
-      if (currentSong !== song) {
+      song = song || SongPlayer.currentSong;
+      if (SongPlayer.currentSong !== song) {
         setSong(song);
         playSong(song);
-      } else if (currentSong === song) {
+      } else if (SongPlayer.currentSong === song) {
         if (currentBuzzObject.isPaused()) {
           playSong(song);
         }
       }
     };
+
     /**
     *@function SongPlayer.pause
-    *@desc Pauuses audio file currentBuzzObject
+    *@desc Pauses audio file currentBuzzObject
     *@param {Object} song
     *@returns {Object} SongPlayer
     */
     SongPlayer.pause = function(song) {
+      song = song || SongPlayer.currentSong;
       currentBuzzObject.pause();
       song.playing = false;
     }
+
+    /**
+    *@function SongPlayer.previous
+    *@desc Gets the index of the currently playing song and increases the index by one.  If index < 0 the currently
+    *playing song will stop and the value will be sent to the first song in the list of songs.  Otherwise the previous
+    *song will begin to play
+    */
+    SongPlayer.previous = function() {
+      var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+      currentSongIndex--;
+
+      if (currentSongIndex < 0) {
+        currentBuzzObject.stop();
+        SongPlayer.currentSong.playing = null;
+      } else {
+        var song = currentAlbum.song[currentSongIndex];
+        setSong(song);
+        playSong(song);
+      }
+    };
 
     return SongPlayer;
   }
 
   angular
     .module('blocJams')
-    .factory('SongPlayer', SongPlayer);
+    .factory('SongPlayer', ['Fixtures', SongPlayer]);
 })();
 
