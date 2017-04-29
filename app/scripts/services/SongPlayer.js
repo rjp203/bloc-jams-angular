2549f58c260f57d3e19ba00a90be53465bb46c62
Ryan J. Popovic <Ryan J. Popovic>
2017-04-29 09:49:17 -0400

    Assignment 7 Completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,18 +1,21 @@
 (function() {
   function SongPlayer() {
     var SongPlayer = {};
-
+    /**
+    *@desc Currently selected song
+    *@param {Object}
+    */
     var currentSong = null;
-  /**
- * @desc Buzz object audio file
- * @type {Object}
- */
+    /**
+    *@desc Buzz object audio file
+    * @type {Object}
+    */
     var currentBuzzObject = null;
- /**
- * @function setSong
- * @desc Stops currently playing song and loads new audio file as currentBuzzObject
- * @param {Object} song
- */
+    /**
+    * @function setSong
+    * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+    * @param {Object} song
+    */
     var setSong = function(song) {
       if (currentBuzzObject) {
         currentBuzzObject.stop();
@@ -26,19 +29,37 @@
 
       currentSong = song;
     }
-
+    /**
+    *@function playSong
+    *@desc Plays audio file currentBuzzObject
+    *@param {Object} song
+    */
+    var playSong = function(song) {
+      currentBuzzObject.play();
+      song.playing = true;
+    }
+    /**
+    *@function SongPlayer.play
+    *@desc If current song is not the song user clicks on, the clicked song becomes current song and plays.
+    *If the current song is the song user clicked on and song is paused, the song will play
+    *@param {Object} song
+    */
     SongPlayer.play = function(song) {
       if (currentSong !== song) {
         setSong(song);
-        currentBuzzObject.play();
-        song.playing = true;
+        playSong(song);
       } else if (currentSong === song) {
         if (currentBuzzObject.isPaused()) {
-          currentBuzzObject.play();
+          playSong(song);
         }
       }
     };
-
+    /**
+    *@function SongPlayer.pause
+    *@desc Pauuses audio file currentBuzzObject
+    *@param {Object} song
+    *@returns {Object} SongPlayer
+    */
     SongPlayer.pause = function(song) {
       currentBuzzObject.pause();
       song.playing = false;
